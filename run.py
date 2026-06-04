465df1f7b43093cdbeeabe2c548277d6b8ab943d
sutharsan <theepansutharsan1117@gmail.com>
2026-06-04 12:48:29 +0530

    change port 8080

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     with app.app_context():
         from app.extensions import db
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)
