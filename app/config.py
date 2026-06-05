66709f43eea2905380c452812c58dafa7c57a61b
sutharsan <theepansutharsan1117@gmail.com>
2026-06-05 13:02:02 +0530

    test db name

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "root123")
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_NAME = os.getenv("DB_NAME", "student_db")
+    DB_NAME = os.getenv("DB_NAME", "railway")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
